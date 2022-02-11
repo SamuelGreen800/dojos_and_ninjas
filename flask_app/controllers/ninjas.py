fe715432ed8d10abee9af625770e420df6d213ac
Samuel Green <samuelgreen800@tutanota.com>
2022-02-11 10:14:43 -0500

    green_samuel_dojos_ninjas_final

---
+++ flask_app/controllers/ninjas.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, request
+from flask_app import app
+from flask_app.models import dojo, ninja
+
+@app.route('/ninjas')
+def ninjas():
+    
+    return render_template('ninja.html',dojos= dojo.Dojo.get_all())
+
+
+@app.route('/create/ninja',methods=['POST'])
+def create_ninja():
+    ninja.Ninja.save(request.form)
+    return redirect('/')
+
+@app.route('/dojo/delete/<int:id>')
+def delete(id):
+    data = {'id':id}
+    ninja.Ninja.delete(data)
+    return redirect('/')
